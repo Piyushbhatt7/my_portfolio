e8a95cf1734978f4c114490e6a0d4d0b1397c21c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-26 03:05:01 +0530

    home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -50,59 +50,7 @@ class _HomePageState extends State<HomePage> {
 
               // const MainDesktop(),
 
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
-                height: screeenHeight,
-                constraints: BoxConstraints(minHeight: 560),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // avatar img
-                    ShaderMask(
-                      shaderCallback: (bounds)
-                    {
-                       return LinearGradient(
-                       colors: [
-                         CustomColor.scaffoldBg.withOpacity(0.6),
-                         CustomColor.scaffoldBg.withOpacity(0.6),
-                       ]
-                       ).createShader(bounds);
-                    },
-                        blendMode: BlendMode.srcATop,
-                        child: Image.asset(
-                          "assets/android.png",
-                          width: screenWidth,
-                          height: 190,
-                        )
-                    ),
-                    // intro text
-
-                    const SizedBox(height: 15,),
-
-                    const Text("Hii, \nI'm Piyush Bhatt\n a Flutter Developer",
-                      style: TextStyle(
-                        fontSize: 24,
-                        height: 1.5,
-                        fontWeight: FontWeight.bold,
-                        color: CustomColor.whitePrimary,
-                      ),
-                    ),
-
-                    const SizedBox(height: 15,),
-
-                    // contact btn
-                    SizedBox(
-                      width: 190,
-                      child: ElevatedButton(onPressed: () {
-
-                      },
-                          child: const Text("Get in touch")
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              
 
               // SKILLS
               Container(
---
+++ lib/widgets/main_mobile.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class MainMobile extends StatelessWidget {
+  const MainMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenHeight = screenSize.height;
+    final screenWidth = screenSize.width;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
+      height: screenHeight,
+      constraints: BoxConstraints(minHeight: 560),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // avatar img
+          ShaderMask(
+              shaderCallback: (bounds)
+              {
+                return LinearGradient(
+                    colors: [
+                      CustomColor.scaffoldBg.withOpacity(0.6),
+                      CustomColor.scaffoldBg.withOpacity(0.6),
+                    ]
+                ).createShader(bounds);
+              },
+              blendMode: BlendMode.srcATop,
+              child: Image.asset(
+                "assets/android.png",
+                width: screenWidth,
+                height: 190,
+              )
+          ),
+          // intro text
+
+          const SizedBox(height: 30,),
+
+          const Text("Hii, \nI'm Piyush Bhatt\n a Flutter Developer",
+            style: TextStyle(
+              fontSize: 24,
+              height: 1.5,
+              fontWeight: FontWeight.bold,
+              color: CustomColor.whitePrimary,
+            ),
+          ),
+
+          const SizedBox(height: 15,),
+
+          // contact btn
+          SizedBox(
+            width: 190,
+            child: ElevatedButton(onPressed: () {
+
+            },
+                child: const Text("Get in touch")
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
