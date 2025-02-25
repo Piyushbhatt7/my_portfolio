a25456324129967ae4f51e5b3347c54ba0916b94
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-26 00:18:52 +0530

    Portfolio website

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_gl_macos
+import path_provider_foundation
+import rive_common
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+}
