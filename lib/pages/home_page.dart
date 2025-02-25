678b8c9e2a448fe59892a637a406a500b51c0753
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-26 00:52:25 +0530

    home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,28 +13,49 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColor.scaffoldBg,
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
           // MAIN
           Container(
-            height: 500,
+            height: 60.0,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
             width: double.maxFinite,
-            color: Colors.blueGrey,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors:
+                  [
+                    Colors.transparent,
+                    CustomColor.bgLight1
+                  ],
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Row(
+              children: [
+                Text("Piyush"),
+                Spacer(),
+                for(int i = 0; i < 5; i++)
+                TextButton(onPressed: (){},
+                    child: Text("button")
+                )
+              ],
+            ),
           ),
 
           // SKILLS
           Container(
             height: 500,
             width: double.maxFinite,
-
+            color: Colors.blueGrey,
           ),
 
           // PROJECTS
           Container(
             height: 500,
             width: double.maxFinite,
-            color: Colors.blueGrey,
+
           ),
 
 
@@ -41,14 +63,14 @@ class _HomePageState extends State<HomePage> {
           Container(
             height: 500,
             width: double.maxFinite,
-            
+            color: Colors.blueGrey,
           ),
 
           // FOOTER
           Container(
             height: 500,
             width: double.maxFinite,
-            color: Colors.blueGrey,
+
           ),
         ],
       )
