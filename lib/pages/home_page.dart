39e3e440dc861a2d53b0cf13fbdfc0300076af50
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-26 02:43:12 +0530

    home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:portfolio/constants/size.dart';
 import 'package:portfolio/styles/style.dart';
 import 'package:portfolio/widgets/header_desktop.dart';
 import 'package:portfolio/widgets/header_mobile.dart';
+import 'package:portfolio/widgets/main_desktop.dart';
 import 'package:portfolio/widgets/site_logo.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
+    final screeenHeight = screenSize.height;
     return LayoutBuilder(
       builder: (context, constraints)
     {
@@ -46,28 +48,11 @@ class _HomePageState extends State<HomePage> {
                   },
                 ),
 
+              // const MainDesktop(),
+
               Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20.0),
-                height: screenSize.height/1.2,
-                constraints: BoxConstraints(minHeight: 350),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Text("Hii, \nI'am Piyush Bhatt\n a Flutter Developer"),
-                        ElevatedButton(onPressed: ()
-                            {
-                              
-                            },
-                            child: const Text("Get in touch")
-                        )
-                      ],
-                    ),
-                    Image.asset("assets/android.png", width: screenWidth/2, height: 160,)
-                  ],
-                ),
-              ),
+                height: scree,
+              )
 
               // SKILLS
               Container(
---
+++ lib/widgets/main_desktop.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class MainDesktop extends StatelessWidget {
+  const MainDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
+    
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          child:
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            height: screenHeight / 1.2,
+            constraints: const BoxConstraints(minHeight: 350.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Hii, \nI'm Piyush Bhatt\n a Flutter Developer",
+                      style: TextStyle(
+                        fontSize: 30,
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColor.whitePrimary,
+                      ),
+                    ),
+
+                    const SizedBox(height: 15,),
+                    SizedBox(
+                      width: 230,
+                      child: ElevatedButton(onPressed: () {
+
+                      },
+                          child: const Text("Get in touch")
+                      ),
+                    )
+                  ],
+                ),
+                Image.asset(
+                  "assets/android.png", width: screenWidth / 2, height: 160,)
+              ],
+            ),
+          );
+
+        }
+    );
+
+  }
+}
