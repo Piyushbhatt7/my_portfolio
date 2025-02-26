3cf0de28e5ba34f0d3685465ff55cf3139d9377b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-26 15:02:24 +0530

    adding skills for desktop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -76,63 +76,8 @@ class _HomePageState extends State<HomePage> {
                   const  SizedBox(height: 50.0,),
                     // platorm and skill
 
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // platforms
 
-                        ConstrainedBox(
-                          constraints: const BoxConstraints(
-                            maxWidth: 450
-                          ),
-                          child: Wrap(
-                            spacing: 5.0,
-                            runSpacing: 5.0,
-                            children: [
-                              for(int i = 0; i < platformItems.length; i++)
-                              Container(
-                                width: 200,
-                                decoration: BoxDecoration(
-                                  color: CustomColor.bgLight2,
-                                  borderRadius: BorderRadius.circular(5),
-                                ),
-
-                                child: ListTile(
-                                  contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                                  leading: Image.asset(platformItems[i]["img"], width: 35, height: 40,),
-                                  title: Text(platformItems[i]["title"]),
-                                 ),
-                              )
-                            ],
-                          ),
-                        ),
-
-                       const SizedBox(width: 50.0,),
-                        // skills
-
-                        Flexible(
-                          child: ConstrainedBox(
-                            constraints: const BoxConstraints(
-                              maxWidth: 500,
-                            ),
-                            child: Wrap(
-                              spacing: 10,
-                              runSpacing: 10,
-                              children: [
-                                for(int i = 0; i < skillItems.length; i++)
-                                Chip(
-                                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                                    label: Text(skillItems[i]["title"]),
-                                  avatar: Image.asset(skillItems[i]["img"]),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+                                     ],
                 ),
               ),
 
