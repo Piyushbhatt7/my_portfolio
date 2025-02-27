3e81b9222e57b2e323895d309d4990022e3535ad
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-27 14:21:59 +0530

    home screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -155,7 +155,9 @@ class _HomePageState extends State<HomePage> {
                             padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
                             child: Row(
                               children: [
-                                 Text("Available on: ", style: ,)
+                                 Text("Available on: ", style: TextStyle(
+
+                                 ),)
                               ],
                             ),
                           )
