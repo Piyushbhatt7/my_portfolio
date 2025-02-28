1631846994f975f7830b2ef1b12c3706d951daf3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-28 21:36:37 +0530

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
             children: [ 
 
               // MAIN 
-/*
+/*  
 
               if(constraints.maxWidth >= kMinDesktopWidth)
                 const HeaderDesktop() else
