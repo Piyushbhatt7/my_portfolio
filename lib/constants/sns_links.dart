74be8ebda78ae640fdda61677cb56ff9d0c3a621
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-02 15:15:17 +0530

    creating Sns file for adding backlinks

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -1,5 +1,5 @@
 class SnsLinks {
 
   static const String github = "https://github.com/Piyushbhatt7";
-  static const String linkdin = "https://www.linkedin.com/feed/";
+  static const String linkdin = "https://www.linkedin.com/in/piyush-bhatt-024221253/";
 }
