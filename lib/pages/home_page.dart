a89dc7f92aefd4b649eb4081c75587c9bf5954ca
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-02 14:42:08 +0530

    dding asets for social media

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -181,8 +181,9 @@ class _HomePageState extends State<HomePage> {
                     
                     Wrap(
                       children: [
-                        Image.asset("assets/social/link-din.png", width: 28,),
-                        Image.asset("assets/social/github.png", width: 28,)
+                        Image.asset("assets/social/link-din.png", width: 32,),
+                        const SizedBox(width: 20.0,),
+                        Image.asset("assets/social/github.png", width: 32,)
                       ],
                     )
                   ],
