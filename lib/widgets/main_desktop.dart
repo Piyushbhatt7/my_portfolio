7898399aee6b7fd1d0d98ff95754a7d6378fd61c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-20 22:25:22 +0530

    main desktop

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -5,7 +5,7 @@ import 'package:typing_text/typing_text.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 import '../constants/colors.dart';
-
+  
 class MainDesktop extends StatelessWidget {
   const MainDesktop({super.key});
 
