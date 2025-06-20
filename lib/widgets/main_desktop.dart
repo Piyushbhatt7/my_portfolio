6aaab7ae666550220ff3acecadea3250198c5b6c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-20 22:40:53 +0530

    main desktop

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -6,7 +6,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 
 import '../constants/colors.dart';
   
-class MainDesktop extends StatelessWidget {              
+class MainDesktop extends StatelessWidget {               
   const MainDesktop({super.key});     
  
   @override   
