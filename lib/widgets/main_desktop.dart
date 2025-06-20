c9ec6f698d6b734ecf7cf2fa8cab010a86488324
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-20 22:40:47 +0530

    main desktop

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -8,7 +8,7 @@ import '../constants/colors.dart';
   
 class MainDesktop extends StatelessWidget {              
   const MainDesktop({super.key});     
-
+ 
   @override   
   Widget build(BuildContext context) {   
     final screenSize = MediaQuery.of(context).size;               
